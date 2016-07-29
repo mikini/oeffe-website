b741e5262790832f20ad8645d62ca0668fdc28c6
Mikkel <miki@geeklabs.dk>
2016-07-29 16:47:17 +0200

    Initial commit. Before local leaflet is used.

---
+++ data.js
@@ -0,0 +1,13 @@
+      var leverandoerer = [
+          // http://nominatim.openstreetmap.org/details.php?place_id=2031756
+          {name: "Mel fra Forum", desc: "Alslevvej 10, 6715 Esbjerg N", goods: ["mel"], pos: [55.5606631,8.4728934], url: "http://melfraforum.dk/"}
+          // http://nominatim.openstreetmap.org/details.php?place_id=47938238
+        , {name: "Varde Ådal Lam", desc:"Toftnæsvej 30, 6800 Varde", goods: ["grøntsager", "æg"], pos: [55.59841,8.425469], url: "http://vardeaadallam.dk/"}
+      ];
+
+     var udlevering = [
+          {name: "Midtgård, ØFFE HQ", desc: "Ravnsbjergparken 6, 6710 Esbjerg V", pos: [55.5314459,8.4368876], url: "http://øffe.tk/"}
+        // wget -O- --quiet "nominatim.openstreetmap.org/search?format=json&q=kirkegade 70, 6700 Esbjerg"
+        , {name: "KFUM/KFUK", desc: "Kirkegade 70, 6700 Esbjerg", pos: [55.471686,8.450611], url: "http://esbjerg.kfum-kfuk.dk/"}
+     ];
+
