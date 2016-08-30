dc3f764f591165e99cf98d51969dc0aa4eac92f9
Mikkel Kirkgaard Nielsen <memb_github@mikini.dk>
2016-08-30 23:17:59 +0200

    Transition to full distribution of leaflet, minor changes to page.
    Remove local leaflet files.
    Add script to install leaflet from distribution.
    Use distribution leaflet files.
    Change emphasis from strong to em.
    Add breaks in Bestilling paragraph.

---
+++ install_leaflet.sh
@@ -0,0 +1,17 @@
+# Get and extract leaflet distribtion
+if [ ! -d leaflet ]; then
+    mkdir leaflet
+    cd leaflet
+    wget http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.zip
+    unzip leaflet.zip
+    rm leaflet.zip
+else
+    echo leaflet seems to be present.
+fi
+
+#mkdir -p images
+#for img in marker-{shadow,icon}.png; do
+#  wget -nc http://cdn.leafletjs.com/leaflet/v0.7.7/images/$img -O images/$img
+#done
+#wget -nc http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css
+#wget -nc http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js
